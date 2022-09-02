be42d8d91a53c607a8385f2d253c6e7eed41d82b
thamzayev <thamzayev@ada.edu.az>
2022-09-02 14:28:55 +0400

    Small visual changes

--- src/page/router.jsx
+++ src/page/router.jsx
@@ -1,28 +1,22 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from "react-router-dom";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 
-// for more information on react router: https://v5.reactrouter.com/web/guides/quick-start
-
 const RouterPage = (props) => {
-    return (
-        <Router basename={props.pageInfo.basePath}>
-            <Switch>
-                <Route path="/page/:pageId">
-                    <Home {...props} />
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router basename={props.pageInfo.basePath}>
+      <Switch>
+        <Route path="/page/:pageId">
+          <Home {...props} />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 RouterPage.propTypes = {
-    pageInfo: PropTypes.object
+  pageInfo: PropTypes.object
 };
 
 export default RouterPage;
